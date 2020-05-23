3cf35394b0f06aeaba2299ed1e46dd35d10bcc39
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2020-05-23 07:49:35 +0530

    array sort 0.1

---
+++ SortArray.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class SortArray{
+	public static void main(String args[]){
+		int[] x={12,34,1,67,98,87,56,76,5,3,45,44,6,76,2};
+		System.out.println(Arrays.toString(x));
+		Arrays.sort(x);
+		System.out.println(Arrays.toString(x));
+	}
+}
